4e9944a7f31a25fbb8ac157f21d6a0da05e2c9fa
Harry <poripori1030@gmail.com>
2018-12-12 06:22:14 +0800

    Settings From Rows

---
+++ TrueLove/Views/SettingsTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  SettingsTableViewCell.swift
+//  TrueLove
+//
+//  Created by Harry on 12/12/2018.
+//  Copyright © 2018 Harry Ng. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+    
+    class SettingsTextField: UITextField {
+        override func textRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override func editingRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override var intrinsicContentSize: CGSize {
+            return .init(width: 0, height: 44)
+        }
+    }
+    
+    let textField: UITextField = {
+        let tf = SettingsTextField()
+        tf.placeholder = "Enter Name"
+        return tf
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(textField)
+        textField.fillSuperview()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+
+}
